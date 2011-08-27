bcaaff5d10d4494aa3283ab6a079bf2fd7e04d9a
Stefano Rivera <stefano@rivera.za.net>
2011-08-27 17:05:27 +0200

    Error page

--- server/router.js
+++ server/router.js
@@ -1,5 +1,21 @@
+fs = require("fs");
+
+function error(response, code) {
+  if(typeof(code) == "undefined")
+    code = 500;
+  response.writeHead(code);
+  fs.readFile('templates/error.html', function(err, data) {
+    response.write(data);
+    response.end();
+  });
+}
+
 function route(pathname, response, postData) {
   console.log("Routing " + pathname);
+  error(response);
+  if (pathname.substring(0, 7) == "/static") {
+    serveStatic(pathname.substring(7), response);
+  }
 }
 
 exports.route = route
