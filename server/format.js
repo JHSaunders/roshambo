f4468fb6cacc53515ff946cb0b4fa098842009e9
Stefano Rivera <stefano@rivera.za.net>
2011-08-27 20:40:36 +0200

    Add serveTemplated to server/format.js

--- server/format.js
+++ server/format.js
@@ -1,3 +1,8 @@
+var fs = require("fs");
+var path = require("path");
+
+var conf = require("./conf.js");
+
 exports.format = function() {
     var formatted = arguments[0];
     for (var i = 1; i < arguments.length; i++) {
@@ -6,3 +11,18 @@ exports.format = function() {
     }
     return formatted;
 };
+
+exports.serveTemplated = function() {
+  var args = Array.prototype.slice.call(arguments);
+  var response = args.shift();
+  var template = args.shift();
+  response.setHeader("Content-Type", "text/html; charset=utf8");
+  response.writeHead(200);
+  fs.readFile(path.join(conf.template_dir, template), 'utf8', function(err, data) {
+    data = exports.format(data, args);
+    response.write(data);
+    response.end();
+  });
+}
+
+// vi: set et sta sw=2 ts=2:
