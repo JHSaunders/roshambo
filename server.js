e148ac059d4f47016e57a4147071956b4c86a20a
Stefano Rivera <stefano@rivera.za.net>
2011-08-27 15:41:37 +0200

    Hello World server

---
+++ server.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
+
+# vi: set et sta sw=2 ts=2:
