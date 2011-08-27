cdd7bd6177d5a0e5324147dc384b07a6f9c3f425
Jason Brownbridge <jbrownbridge@gmail.com>
2011-08-27 19:43:16 +0200

    Added nonce.js that I stole and worked on game.js

--- server/game.js
+++ server/game.js
@@ -1,19 +1,36 @@
-var base62 = require('./base62');
-
 MIN_NUMBER = 916132832;
 MAX_NUMBER = 56800235583;
 
-exports.games = {};
+var base62 = require('./base62');
+var nonce  = require('./nonce.js');
+var games  = exports.games = {};
 
 exports.index = function(response) {
   var key = "";
   do {
     var num = MIN_NUMBER + (Math.random() * ((MAX_NUMBER - MIN_NUMBER) + 1));
     key = base62.encode(num);
-  } while (key in exports.games);
+  } while (key in games);
+  games[key] = [];
+  // return response page with key in it for link to play
+  return response;
 };
 
 exports.gamePage = function(response, name) {
+  if (name in games) {
+    nonces = games[name];
+    if (nonces.length < 2) {
+      var n = new Nonce();
+      nonces.push(n);
+      // set cookie and return response
+      response.writeHead(200, {
+        'Set-Cookie'  : 'nonce=' + base62.encode(nonces),
+        'Content-Type': 'text/plain',
+      });
+      return response;
+    }
+  }
+  // Error handling
 };
 
 // vi: set et sta sw=2 ts=2:
