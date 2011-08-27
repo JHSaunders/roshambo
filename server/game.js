d9597ccb8de7dcc3eaead32b15d58a7f36025fab
Jason Brownbridge <jbrownbridge@gmail.com>
2011-08-27 20:08:36 +0200

    Fixed bug with nonce.

--- server/game.js
+++ server/game.js
@@ -2,10 +2,27 @@ MIN_NUMBER = 916132832;
 MAX_NUMBER = 56800235583;
 
 var base62 = require('./base62');
-var nonce  = require('./nonce.js');
 var games  = exports.games = {};
 
-exports.index = function(response) {
+function createNonce() {
+  return base62.encode(new Date().getTime());
+}
+
+function getCookies(request) {
+  // To Get a Cookie
+  var cookies = {};
+  request.headers.cookie && request.headers.cookie.split(';').forEach(function( cookie ) {
+    var parts = cookie.split('=');
+    cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim();
+  });
+  return cookies;
+};
+
+function getNonce(request) {
+  return getCookies(request)['nonce'];
+};
+
+exports.index = function(request, response) {
   var key = "";
   do {
     var num = MIN_NUMBER + (Math.random() * ((MAX_NUMBER - MIN_NUMBER) + 1));
@@ -16,21 +33,36 @@ exports.index = function(response) {
   return response;
 };
 
-exports.gamePage = function(response, name) {
+exports.gamePage = function(request, response, name) {
   if (name in games) {
     nonces = games[name];
     if (nonces.length < 2) {
-      var n = new Nonce();
+      var n = createNonce();;
       nonces.push(n);
       // set cookie and return response
       response.writeHead(200, {
-        'Set-Cookie'  : 'nonce=' + base62.encode(nonces),
+        'Set-Cookie'  : 'nonce=' + n,
         'Content-Type': 'text/plain',
       });
       return response;
     }
   }
   // Error handling
+};
+
+exports.wait = function(request, response, name) {
+  var n = getNonce(request);
+  if (typeof(n) != "undefined" && game[name].length === 2) {
+    var p1 = game[name][0];
+    var p2 = game[name][1];
+  } 
+};
+
+exports.play = function(request, response, name) {
+  var cookies = getCookies(request);
+  if ('nonce' in cookies) {
+    var n = cookies['nonce'];
+  }
 };
 
 // vi: set et sta sw=2 ts=2:
