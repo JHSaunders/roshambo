25aba85bc5070a5377e7eaf71c7e6df48fd1ccfa
Stefano Rivera <stefano@rivera.za.net>
2011-08-27 17:47:32 +0200

    Static file serving

--- server/conf.js
+++ server/conf.js
@@ -1,3 +1,6 @@
 exports.port = 8888;
+exports.project_root = "./";
+exports.template_dir = exports.project_root + "templates/";
+exports.static_dir = exports.project_root + "static/";
 
 // vi: set et sta sw=2 ts=2:
--- server/router.js
+++ server/router.js
@@ -1,20 +1,51 @@
-fs = require("fs");
+var fs = require("fs");
+var path = require("path");
+var conf = require("./conf.js");
 
-function error(response, code) {
+function errorResponse(response, code) {
+  console.log("Serving error " + code);
   if(typeof(code) == "undefined")
     code = 500;
+  response.setHeader("Content-Type", "text/html; charset=utf8");
   response.writeHead(code);
-  fs.readFile('templates/error.html', function(err, data) {
+  fs.readFile(path.join(conf.template_dir, 'error.html'), function(err, data) {
     response.write(data);
     response.end();
   });
 }
 
+var mimeTypes = {
+  "HTML": "text/html",
+  "TXT": "text/plain",
+  "CSS": "text/css",
+  "PNG": "image/png",
+};
+
+function serveStatic(response, pathname) {
+  console.log("Serving static '" + pathname + "'");
+  var localFile = path.join(conf.static_dir, pathname);
+  console.log("Which is '" + localFile + "'");
+  path.exists(localFile, function (exists) {
+    if (!exists) {
+      errorResponse(response, 404);
+      return;
+    }
+    response.setHeader("Content-Type", mimeTypes[path.extname(pathname).substring(1).toUpperCase()]);
+    response.writeHead(200);
+    fs.readFile(localFile, function(err, data) {
+      response.write(data);
+      response.end();
+    });
+  });
+}
+
 function route(pathname, response, postData) {
   console.log("Routing " + pathname);
-  error(response);
-  if (pathname.substring(0, 7) == "/static") {
-    serveStatic(pathname.substring(7), response);
+  if (pathname.substring(0, 8) == "/static/") {
+    serveStatic(response, pathname.substring(8));
+    return
+  } else {
+    errorResponse(response);
   }
 }
 
