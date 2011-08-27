c4f8f27d6007cd449c40e39216c75cffcb391f95
Jason Brownbridge <jbrownbridge@gmail.com>
2011-08-27 17:22:28 +0200

    Added base62 encoder.

---
+++ server/base62.js
@@ -0,0 +1,22 @@
+var ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var BASE = 62;
+
+exports.encode = function(num) {
+  var str = "";
+  do {
+    var m = num % BASE;
+    num = Math.floor(num / BASE);
+    str = ALPHABET[m] + str;
+  } while (num > 0);
+  return str;
+}
+
+exports.decode = function(string) {
+  var len = string.length;
+  var num = 0;
+  for (i = 0; i < len; i++) {
+    var power = len - (i + 1);
+    num = num + ALPHABET.indexOf(string[i]) * Math.pow(BASE, power);
+  }
+  return num;
+}
